485568783cfea3de5de96384ddc090d66bb1fd29
PhillipMagos <phillipmagos@gmail.com>
2023-07-18 22:58:06 -0500

    first

---
+++ app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+const Contact = () => {
+  return (
+    <div>
+      Contact
+    <Image src="/public/pics/two.png" className="" alt="..." width={500} height={300}></Image>
+    </div>
+  )
+}
+
+export default Contact
