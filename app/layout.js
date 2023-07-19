3167a23e239ebc0a30981ebf770986c64a0f8b33
PhillipMagos <phillipmagos@gmail.com>
2023-07-18 22:59:40 -0500

    second

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from "./components/navbar"
+import Footer from "./components/footer"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <body className={inter.className}>
+      <Navbar />
+      <main className="container">
+        <div className="flex items-start justify-center min-h-screen">
+          <div className='mt-20'>{children}</div>
+        </div>
+      </main>
+      <Footer />
+    </body>
+  </html>
   )
 }
