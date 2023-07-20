6c3856faa3290be252a1c681f413cb369076a73b
PhillipMagos <phillipmagos@gmail.com>
2023-07-20 10:10:29 -0500

    api test

---
+++ app/apiTest/page.jsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react'
+import Posts from './Posts'
+
+
+
+const apiTest = () => {
+  return (
+    <div>apiTest
+
+        <Posts />
+    </div>
+    
+  )
+}
+
+export default apiTest
